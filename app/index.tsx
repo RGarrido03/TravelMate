09a54ec56afd74a07b6ed4c8f550de25d8aa22e2
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-04-20 12:28:05 +0100

    Add Expo Router, major refactoring
    Co-authored-by: Diana Miranda <dianarrmiranda@users.noreply.github.com>
    Co-authored-by: Diogo Falcão <falcaodiogo@users.noreply.github.com>

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ImageBackground} from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={image} resizeMode="cover">
+                <Text>TravelMate</Text>
+                <StatusBar style="auto" />
+            </ImageBackground>
+        </View>
+    );
+}
+
+const image = {uri: 'https://reactjs.org/logo-og.png'};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
