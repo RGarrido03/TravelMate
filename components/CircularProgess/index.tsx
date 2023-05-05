8268bbf026b3dd820305e3593c898b10da27e74b
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-05-05 01:10:17 +0100

    CircularProgress: dark mode support

--- components/CircularProgess/index.tsx
+++ components/CircularProgess/index.tsx
@@ -1,25 +1,28 @@
 import Svg, { Circle, Path, Text as SvgText, G } from "react-native-svg";
+import { useColorScheme } from "react-native";
 
 export const CircularProgress = ({ percentage, circumference }: Props) => {
     const strokeDasharray: string = `${circumference} ${circumference}`;
     const strokeDashoffset: number = circumference - (circumference * percentage) / 100;
+    const isLightMode: boolean = useColorScheme() === "light";
+
     return (
         <Svg height="60em" width="60em" viewBox="0 0 80 80">
             <G>
-                <Circle cx="40" cy="40" r="36" fill="#FFFFFF" stroke="#D9D9D9" strokeWidth="3" />
+                <Circle cx="40" cy="40" r="36" stroke="#D9D9D9" strokeWidth="3" />
                 <SvgText
-                    x="37"
+                    x="32"
                     y="45"
                     textAnchor="middle"
                     fontSize="16"
                     fontWeight="600"
-                    fill="#3B4949"
+                    fill={isLightMode ? "#3B4949" : "#fff"}
                 >
                     {percentage}%
                 </SvgText>
                 <Path
                     d="M40 4.6322 a 35.3678 35.3678 0 0 1 0 70.7356 a 35.3678 35.3678 0 0 1 0 -70.7356"
-                    stroke="#D9D9D9"
+                    stroke={isLightMode ? "#D9D9D9" : "#555"}
                     strokeWidth="6"
                     strokeLinecap="round"
                     fill="none"
